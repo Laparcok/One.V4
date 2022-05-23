480614a9f2ec69781b883f71db4db3448a44f598
Laparcok <105703266+Laparcok@users.noreply.github.com>
2022-05-23 17:19:29 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'SCRIPT')).buffer(), `
 SCRIPT BOT
-`.trim(), 'https://github.com/davidpangrib001/One-V4\nJangan Lupa Kasih Stars 😁', 'Pemilik Bot', '#owner')
+`.trim(), 'Cari sc \nDi github banh:v', 'Pemilik Bot', '#owner')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
